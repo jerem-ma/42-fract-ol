bda7e3c05f3b73358730f6813c07537de13ca368
Jeremy Maia <jmaia@student.42.fr>
2021-12-10 12:52:16 +0100

    Norminetize main

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,11 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 12:47:02 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/10 12:47:39 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/10 12:52:03 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	(void) ac;
 	(void) av;
