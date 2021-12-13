fbb6a5d8117fdec5af06197429e74cc562ace718
Jeremy Maia <jmaia@student.42.fr>
2021-12-13 17:35:09 +0100

    For posterity, waste of time trying to convert str to double

--- srcs/ft_floats.c
+++ srcs/ft_floats.c
@@ -6,16 +6,79 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:59:35 by jmaia             #+#    #+#             */
-/*   Updated: 2021/12/13 14:07:16 by jmaia            ###   ########.fr       */
+/*   Updated: 2021/12/13 17:33:01 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_floats.h"
+#include <math.h>
 
 double	parse_double(char *str)
 {
-	(void) str;
-	return (0.0);
+	int		integer_part;
+	int		float_part;
+	char	*float_part_ptr;
+	double	result;
+
+	float_part_ptr = ft_strchr(str, '.') + 1;
+	integer_part = ft_atoi(str);
+	if (float_part_ptr == 0)
+		return ((double) integer_part);
+	float_part = ft_atoi(float_part_ptr);
+	result = integer_part;
+	result += float_part / pow(10, ft_strlen(float_part_ptr));
+	return (result);
+}
+
+//int	set_int_mantissa(double *nbr, unsigned long int integer_part)
+//{
+//	int	i;
+//	int	count;
+//	int	digit;
+//
+//	i = 63;
+//	count = 0;
+//	while (i >= 0 && count < 52)
+//	{
+//		digit = 1 << i & integer_part;
+//		if (digit || count)
+//		{
+//			*nbr |= (!!digit) << (51 - count);
+//			count++;
+//		}
+//		i--;
+//	}
+//}
+//
+//int	set_float_mantissa(double *nbr, unsigned long int float_part, int shift)
+//{
+//	int	i;
+//	int	count;
+//	int	digit;
+//
+//	i = 63;
+//	count = shift;
+//	while (i >= 0 && count < 52)
+//	{
+//		digit = 1 << i & integer_part;
+//		if (digit || count)
+//		{
+//			*nbr |= (!!digit) << (51 - count);
+//			count++;
+//		}
+//		i--;
+//	}
+//}
+
+int main()
+{
+	double ft_nbr;
+	double nbr;
+	char *str = "2424245.123612315";
+
+	ft_nbr = parse_double(str);
+	nbr = strtod(str, 0);
+	printf("%f %f\n", ft_nbr, nbr);
 }
 
 int	is_float(char *str)
